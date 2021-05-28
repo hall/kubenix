59da5c1698ee7730d3ec7523011215e86a4340dc
David Arnold <dar@xoe.solutions>
2021-05-28 14:03:46 -0500

    ref: don't expose upstreamable lib a second time as kubenix.lib
    It is already overlayd onto pkgs.lib in the context of kubenix'
    evalModules

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 { lib, pkgs }:
 
-(import ./extra.nix { inherit pkgs lib; }) // {
+{
   k8s = import ./k8s { inherit lib; };
   docker = import ./docker { inherit lib pkgs; };
   helm = import ./helm { inherit pkgs; };
