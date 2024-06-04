06a7ddb5f8b57f08dcf373c2c1d4f2ea71027655
Cody McGinnis <cody@mcginnis.dev>
2024-06-04 02:15:27 +0000

    allow kubeconfig to not be set

--- pkgs/kubenix.nix
+++ pkgs/kubenix.nix
@@ -38,7 +38,7 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/kubenix \
       --set PATH "$out/bin" \
-      --run 'export KUBECONFIG=''${KUBECONFIG:-${kubeconfig}}' \
+      --run 'export KUBECONFIG=''${KUBECONFIG:-${toString kubeconfig}}' \
       --set KUBECTL_EXTERNAL_DIFF '${diff}' \
       --set MANIFEST '${result}'
   '';
