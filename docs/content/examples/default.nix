9cb8c16a13f896dc2a0a2751940990ce0318b1d3
Jairo Llopis <jairo@moduon.team>
2026-02-16 12:00:11 +0000

    tests(examples): check all of them
    Most examples were skipped. Now all are checked with a simple `nix flake check`.

--- docs/content/examples/default.nix
+++ docs/content/examples/default.nix
@@ -1,5 +1,9 @@
 {
   deployment = import ./deployment { };
+  helm = import ./helm { };
+  image = import ./image { };
+  namespaces = import ./namespaces { };
+  pod = import ./pod { };
   testing = import ./testing { };
   custom-resources = import ./custom-resources { };
 }
