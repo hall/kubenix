88af6e56ac1e9c1c222a8671babd9a555c41a07b
Simon Shine <simon@simonshine.dk>
2026-07-15 14:23:57 +0200

    chore(flake): move apps to flake/apps.nix

--- flake.nix
+++ flake.nix
@@ -28,55 +28,6 @@
     {
       nixosModules.kubenix = import ./modules;
 
-      apps = eachSystem (pkgs: {
-        docs = {
-          type = "app";
-          meta = {
-            description = "Generate and build the Kubenix documentation site using Hugo";
-          };
-          program = (pkgs.writeShellScript "gen-docs" ''
-            set -eo pipefail
-
-            # generate json object of module options
-            nix build '.#docs'
-
-            # copy file to avoid symlink in resulting build artifacts
-            cp -f ./result ./docs/data/options.json
-
-            # remove all old module pages
-            rm ./docs/content/modules/[!_]?*.md || true
-
-            # create a page for each module in hugo
-            for mod in ${builtins.toString (builtins.attrNames self.nixosModules.kubenix)}; do
-              [[ $mod == "base" ]] && mod=kubenix
-              [[ $mod == "k8s" ]] && mod=kubernetes
-              echo "&nbsp; {{< options >}}" > ./docs/content/modules/$mod.md
-            done
-
-            # build the site
-            cd docs && ${pkgs.hugo}/bin/hugo "$@"
-          '').outPath;
-        };
-
-        generate = {
-          type = "app";
-          meta = {
-            description = "Generate Nix modules from Kubernetes specifications and CRDs";
-          };
-          program = (pkgs.writeShellScript "gen-modules" ''
-            set -eo pipefail
-            dir=./modules/generated
-
-            rm -rf $dir
-            mkdir $dir
-            nix build '.#generate-k8s'
-            cp ./result/* $dir/
-
-            rm result
-          '').outPath;
-        };
-      });
-
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
