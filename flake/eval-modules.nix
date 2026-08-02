ddbee5284356054bbf02f02cfac40da5b5f2435d
Simon Shine <simon@simonshine.dk>
2026-08-02 10:37:50 +0200

    refactor(flake): make evalModules self-recursive
    - The nested evaluator handed to modules was self.evalModules.<system>, which is this same
      function; binding it by name instead removes the flake's last hold on the evaluator body.
    - Keeps the nested evaluator on the same pkgs as its parent, rather than re-deriving it from a
      system string.
    
    All check and package derivations are unchanged.

--- flake/eval-modules.nix
+++ flake/eval-modules.nix
@@ -4,35 +4,37 @@ let
   mkEvalModules = system:
     let
       pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in
-    attrs @ { module ? null, modules ? [ module ], ... }:
-    let
-      lib' = pkgs.lib.extend (lib: _self: import ../lib/upstreamables.nix { inherit lib pkgs; });
-      attrs' = builtins.removeAttrs attrs [ "module" ];
-    in
-    lib'.evalModules (pkgs.lib.recursiveUpdate
-      {
-        modules = modules ++ [{
-          config._module.args = {
-            inherit pkgs;
-            name = "default";
-          };
-        }];
-        specialArgs = {
-          pkgs = import inputs.nixpkgs {
-            inherit (pkgs.stdenv.hostPlatform) system;
-            overlays = [ self.overlays.default ];
-            config.allowUnsupportedSystem = true;
-          };
 
-          kubenix = {
-            lib = import ../lib { inherit pkgs; inherit (pkgs) lib; };
-            evalModules = self.evalModules.${pkgs.stdenv.hostPlatform.system};
-            modules = import ../modules;
-          };
-        };
-      }
-      attrs');
+      evalModules = attrs @ { module ? null, modules ? [ module ], ... }:
+        let
+          lib' = pkgs.lib.extend (lib: _self: import ../lib/upstreamables.nix { inherit lib pkgs; });
+          attrs' = builtins.removeAttrs attrs [ "module" ];
+        in
+        lib'.evalModules (pkgs.lib.recursiveUpdate
+          {
+            modules = modules ++ [{
+              config._module.args = {
+                inherit pkgs;
+                name = "default";
+              };
+            }];
+            specialArgs = {
+              pkgs = import inputs.nixpkgs {
+                inherit (pkgs.stdenv.hostPlatform) system;
+                overlays = [ self.overlays.default ];
+                config.allowUnsupportedSystem = true;
+              };
+
+              kubenix = {
+                lib = import ../lib { inherit pkgs; inherit (pkgs) lib; };
+                inherit evalModules;
+                modules = import ../modules;
+              };
+            };
+          }
+          attrs');
+    in
+    evalModules;
 in
 {
   flake.evalModules = inputs.nixpkgs.lib.genAttrs config.systems mkEvalModules;
